2298e18e50ec9db2a4824c0ab92db386a5f84019
frankyork <frankgv.2015@gmail.com>
2016-03-17 16:26:27 -0500

    poniendo php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,127 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Yuliño Elman Estela Martinez</title>
+	
+<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="4.mp4">NETYUL</a
+			</div>
+			<<body>
+			 <background red
+			</body>			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">BIENBENIDO A UN LINDO VIAJE</a></li>
+					<li><a href="#"></a></li>
+				</ul>
+				<nav class="ed-item base-70 main-end cross-center">
+				<div id="search-box" class="main-search-box"> <form method="GET" action="file:///C:/Users/YULI%C3%91O%20ELMAN/Videos/video%20de%20amor/video%20de%20amor.html" accept-charset="UTF-8"><input style="display:none;" type="Descarga" value="Buscar"> </form> </div> 
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control"placeholder="Mas destinos">
+					</div>
+					
+				</form>
+				</nav>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">TUTORIALES DE GUIAS</a></li>
+					<div class="carousel-caption">
+						<h1>VUELA A UN DESTINO MEJOR.</h1>
+						<p>El Perú un Pais de grandes maravillas</p>
+						<p><a class="btn btn-lg btn-primary" a href="https://www.google.com.pe/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=Cusco+Per%C3%BA&stick=H4sIAAAAAAAAAONgFuLUz9U3MDQrrypX4gAxs3JSjLWknfNL80qKKkPyQ_JLizKLS1xSi0sy8xJLMvPzAFOFS_00AAAA">GALERIA DE LAS AGENCIAS TURISTICAS</a></p>
+					</div>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Opciones<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Registrarse</a></li>
+							<li><a href="#">Guias</a></li>
+							<li><a href="#">Profesores de guis</a></li>
+							<li><a href="#">Titulos de hoy guia</a></li>
+						</ul>
+					</li>
+
+
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+</div>
+	<!--slider-->
+	<center>
+	<div id="carousel-id" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img data-src="public/imagenes/00.jpg" alt="First slide" src="public/imagenes/00.jpg" width="911">
+				
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Los líderes viven por elección.</h1>
+						<p></p>
+						<p><a class="btn btn-lg btn-primary" a href="https://www.youtube.com/watch?v=tgFao8RYeQ8" role="button">GUIA TURISTICA DE VIAJES A MACHU PICCHU DEL CUZCO</a></p>
+
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img data-src="public/imagenes/14.jpg" alt="Second slide" src="public/imagenes/14.jpg" width="1058">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Habla de tal manera que otros amen escucharte escucha de tal manera que otros amen hablarte 
+Pedir perdón es de inteligentes, perdonar es de nobles y perdonarse es de sabios no sirve de mucho la riqueza en los bolsillos, cuando hay pobreza en el corazón..</h1>
+						<p>.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">FRASES PERUANAS DE HOY EN DIA</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item active">
+				<img data-src="public/imagenes/33.jpg" alt="Third slide" src="public/imagenes/33.jpg" width="1058">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>VUELA A UN DESTINO MEJOR</h1>
+						<p>El Perú un Pais de grandes maravillas</p>
+						<p><a class="btn btn-lg btn-primary" a href="https://www.google.com.pe/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=Cusco+Per%C3%BA&stick=H4sIAAAAAAAAAONgFuLUz9U3MDQrrypX4gAxs3JSjLWknfNL80qKKkPyQ_JLizKLS1xSi0sy8xJLMvPzAFOFS_00AAAA">GALERIA DE LAS AGENCIAS TURISTICAS</a></p>
+					</div>
+				</div>
+
+			</div>            
+		</div>
+		<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div>
+	</center>
+	
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js">
+</script>
+</body>
+</html>
+<the <body>
+<the <body>
+<<tr><div align=""> <iframe width="0" height="0" 
+      src="88.mp4" frameborder="" allowfullscreen=""></tr>
+</body>
+
